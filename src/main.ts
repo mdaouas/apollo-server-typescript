15cce37627b4fd3680fcdc47a081eb52c749ec87
mdaouas <mehdi.daouas@bluewin.ch>
2019-02-10 17:55:46 +0100

    Starting point for a typescript Apollo GraphQL Server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-// console.log('Hello World!');
-console.log('foo bar');
+import { ApolloServer } from "apollo-server";
+
+import { environment } from "./environment";
+import resolvers from "./resolvers";
+import typeDefs from "./schemas";
+
+const server = new ApolloServer({resolvers, typeDefs, introspection: environment.apollo.introspection, playground: environment.apollo.playground});
+
+server.listen(environment.port).then(({url}) => console.log(`Server ready at ${url}. `));
 
 // Hot Module Replacment
 if (module.hot) {
     module.hot.accept();
-    module.hot.dispose(() => console.log('Module disposed. '));
+    module.hot.dispose(() => server.stop());
 }
